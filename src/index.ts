caa944aaf4de9155422ff8d42a1ee021563dc057
Rel1cx <rel1cx@proton.me>
2025-01-29 14:04:54 +0800

    refactor: replace 'hermes-eslint' with 'meriyah' and add '@typescript-eslint/scope-manager'

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,74 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import tsBlankSpace from "ts-blank-space";
-// @ts-expect-error hermes-eslint missing ts types
-import { parse as _parse, parseForESLint as _parseForESLint } from "hermes-eslint";
+import * as meriyah from "meriyah";
+import { analyze } from "@typescript-eslint/scope-manager";
 import packageJson from "../package.json" with { type: "json" };
 
 const { name, version } = packageJson;
 
-function parse(code: string, options?: Record<string, any>) {
-  return _parse(tsBlankSpace(code), options);
+const defaultOptions: meriyah.Options = {
+  // The flag to allow module code
+  module: true,
+
+  // The flag to enable stage 3 support (ESNext)
+  next: false,
+
+  // The flag to enable start, end offsets and range: [start, end] to each node
+  ranges: true,
+
+  // Enable web compatibility
+  webcompat: false,
+
+  // The flag to enable line/column location information to each node
+  loc: true,
+
+  // The flag to attach raw property to each literal and identifier node
+  raw: true,
+
+  // The flag to allow return in the global scope
+  globalReturn: false,
+
+  // The flag to enable implied strict mode
+  impliedStrict: true,
+
+  // Allows comment extraction. Accepts either a function or array
+  // onComment: [],
+
+  // Allows detection of automatic semicolon insertion. Accepts a callback function that will be passed the character offset where the semicolon was inserted
+  // onInsertedSemicolon: (pos) => { },
+
+  // Allows token extraction. Accepts either a function or array
+  // onToken: [],
+
+  // Enable non-standard parenthesized expression node
+  // preserveParens: false,
+
+  // Enable lexical binding and scope tracking
+  // lexical: false,
+
+  // Adds a source attribute in every node’s loc object when the locations option is `true`
+  // source: undefined, // Set to source: 'source-file.js'
+
+  // Enable React JSX parsing
+  jsx: true,
+};
+
+function parse(code: string, options?: meriyah.Options) {
+  return meriyah.parse(tsBlankSpace(code), { ...defaultOptions, ...options });
 }
 
-function parseForESLint(code: string, options?: Record<string, any>) {
-  return _parseForESLint(tsBlankSpace(code), options);
+function parseForESLint(code: string, options?: meriyah.Options) {
+  const opts = { ...defaultOptions, ...options };
+  const ast = meriyah.parse(tsBlankSpace(code), opts);
+  const scopeManager = analyze(ast as never, {
+    impliedStrict: !!opts.impliedStrict,
+    sourceType: opts.module
+      ? "module"
+      : "script",
+  });
+  return {
+    ast,
+    scopeManager,
+  };
 }
 
 export default {
